49bfbe738dac7a16983f92355c5a9d59a469ce22
Paul Sempels <paulsempels@gmail.com>
2021-02-08 22:53:25 +0100

    merged js with html/css, some major bugfixes too

--- broken pokedex.js
+++ broken pokedex.js
@@ -1,5 +1,5 @@
 // Globals //
-const baseAPILink = "https://pokeapi.co/api/v2/pokemon/";
+const baseAPILink = "https://pokeapi.co/api/v2/pokemon/bulbusaur";
 const totalPokemon = 898;
 let APILink = "https://pokeapi.co/api/v2/pokemon/1";
 
@@ -11,29 +11,26 @@ const nextP = document.querySelector("#btn-r");
 
 // input //
 const pByInput = document.querySelector("#text-num"); // accepts both ID and Name to browse the API.
-//functions//
 
+// functions //
 async function fetchPokemons(APILink) {
     try {
         let pResult = await fetch(APILink);
         let pData = await pResult.json();
-        console.log(pData);
-        displayPokemon(pData);
-        await fetchSpecies(pData);
         return pData;
     } catch (error) {
         console.error(error);
         console.log("all the pokemon just died because of you")
     }
-
+    return pData;
 }
 
 async function fetchSpecies(pData) {
     try {
+        console.log("test2")
         let pSResult = await fetch(pData.species.url);
         let pSData = await pSResult.json();
-        await fetchEvolutions(pSData)
-        console.log(pSData);
+        return pSData
     } catch (error) {
         console.error(error);
         console.log("What have you done?! Why would anyone break all the poké eggs?!")
@@ -42,40 +39,31 @@ async function fetchSpecies(pData) {
 
 async function fetchEvolutions(pSData) {
     try {
+        console.log("test3")
         let pEResult = await fetch(pSData.evolution_chain.url);
         let pEData = await pEResult.json();
-        console.log(pEData);
-        displayEvolutions()
+        //displayEvolutions(pEData);
     } catch (error) {
         console.error(error);
         console.log("Hello? Can anyone hear me?! I've been stuck here since 1995")
     }
 }
 
-async function nextPokemon() {
-    let temp = await fetchPokemons(APILink);
-    let newId = Number(temp.id) + 1;
-    if (newId > totalPokemon) {
-        newId = 1;
+/*function displayEvolutions(pEData) {
+    const pEv0 = document.querySelector("#img-1");
+    const pEv1 = document.querySelector("#img-2");
+    const pEv2 = document.querySelector("#img-3");
+    if (evolution.chain.evolves_to.length === 1) {
+      // let ev0Link =
     }
-    APILink = baseAPILink + newId;
-    fetchPokemons(APILink);
-}
+}*/
 
-async function previousPokemon() {
-    let temp = await fetchPokemons(APILink);
-    console.log(temp.id + " wooooooooooooooooooooooooooooooo")
-    let newId = Number(temp.id) - 1
-    if (newId === 0) {
-        APILink = baseAPILink + totalPokemon;
-        fetchPokemons(APILink);
-    } else {
-        APILink = baseAPILink + newId;
-        fetchPokemons(APILink);
-    }
-}
+function displayPokemon() {
+    pData = fetchPokemons(APILink);
+    console.log("testing" + pData);
+    fetchSpecies(pData);
+    fetchEvolutions(pSData);
 
-function displayPokemon(pData) {
     const pFrontSprite = document.querySelector("#poke-sprite");
     const pTypeOne = document.querySelector("#type1-s");
     const pTypeTwo = document.querySelector("#type2-s");
@@ -83,46 +71,61 @@ function displayPokemon(pData) {
     const pHeight = document.querySelector("#height");
     const pName = document.querySelector("#poke-name");
     const pId = document.querySelector("#poke-id");
-    const pMoves = document.getElementsByClassName("moves");
+    const pMoves = document.querySelectorAll(".moves");
     pFrontSprite.src = pData.sprites.front_default;
     pName.textContent = pData.name;
-    pId.textContent = pData.id.toString().padStart(3, "0");
-    pHeight.innerHTML = `<p>Height: ${pData.height} Poké-units</p>`;
-    pWeight.innerHTML = `<P>Weight: ${pData.weight} Poké-units</p>`;
-    console.log('moves')
+    pId.textContent = pData.id;
+    pHeight.textContent = pData.height;
+    pWeight.textContent = pData.weight;
+
     if (pData.moves.length === 1) {
-        pMoves[0].innerText = pData.moves[0].name;
-        pMoves[1].innerText = "-"
-        pMoves[2].innerText = "-"
-        pMoves[3].innerText = "-"
-    } else if (pData.moves.length === 0) {
-        pMoves[0].innerText = "-"
-        pMoves[1].innerText = "-"
-        pMoves[2].innerText = "-"
-        pMoves[3].innerText = "-"
+        pMoves[0].textContent = pData.moves[0].name;
+        pMoves[1].textContent = "-"
+        pMoves[2].textContent = "-"
+        pMoves[3].textContent = "-"
     } else {
-        for (let i = 0; i < pMoves.length; i++) {
-            pMoves[i].innerText = pData.moves[i].move.name
+        for (let i = 0; i < pMoves; i++) {
+            pMoves[i].textContent = pData.moves[i].name
         }
     }
-    pTypeOne.textContent = "";
-    pTypeTwo.textContent = "";
-    if (pData.types.length === 1) {
-        pTypeOne.textContent = pData.types[0].type.name;
+    if (pData.types.length > 1) {
+        pTypeOne.textContent = pData.types[0].name;
     } else {
-        pTypeOne.textContent = pData.types[0].type.name;
-        pTypeTwo.textContent = pData.types[1].type.name;
+        pTypeOne.textContent = pData.types[0].name;
+        pTypeTwo.textContent = pData.types[1].name;
     }
+    nextP.addEventListener("click", () => {
+        let newId = Number(pData.id) + 1;
+        if (newId > totalPokemon) {
+            newId = 1;
+        }
+        APILink = baseAPILink + newId
+        fetchPokemons(APILink);
+    });
+
+    previousP.addEventListener("click", () => {
+        let newId = Number(pData.id) - 1;
+        if (newId === 0) {
+            newId = totalPokemon;
+        }
+        APILink = baseAPILink + newId
+        fetchPokemons(APILink);
+    });
 
     pByInput.addEventListener("keyup", (event) => {
-        if (event.key === 'Enter') {
+        if (event.code === 'enter') {
             let nameOrId = pByInput.value;
             APILink = baseAPILink + nameOrId;
             fetchPokemons(APILink);
         }
     });
+
 }
 
 // Execution //
-fetchPokemons(APILink);
+displayPokemon();
+
+
+
+
 
